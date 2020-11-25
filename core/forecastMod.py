a3e297c449edf670482efdcb8c73eeab2d9a2ddf
Ryan Grout <ryan.grout@nwc.nws.noaa.gov>
2020-11-25 14:31:57 -0600

    Remove values() call.

--- core/forecastMod.py
+++ core/forecastMod.py
@@ -275,5 +275,5 @@ def process_forecasts(ConfigOptions, wrfHydroGeoMeta, inputForcingMod, suppPcpMo
                 err_handler.log_critical(ConfigOptions, MpiConfig)
             err_handler.check_program_status(ConfigOptions, MpiConfig)
     
-    for in_force in ConfigOptions.input_forcings.values():
+    for in_force in ConfigOptions.input_forcings:
         in_force.regridObj.destroy()
